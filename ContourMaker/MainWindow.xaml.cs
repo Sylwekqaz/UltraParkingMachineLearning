5809f92fc611e7edf5ad322bb290b0dd37917677
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-09-02 20:34:54 +0200

    dodanie opencvsharp, dodanie renderowania bitmap

--- ContourMaker/MainWindow.xaml.cs
+++ ContourMaker/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Windows;
 using System.Windows.Input;
-using ContourMaker.utils;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Window = System.Windows.Window;
 
 namespace ContourMaker
 {
@@ -17,13 +16,9 @@ namespace ContourMaker
             InitializeComponent();
             try
             {
-                BackgroundBitmap = new Bitmap("Images/test1a.jpg");
-                MaskBitmap = new Bitmap(BackgroundBitmap.Width, BackgroundBitmap.Height, PixelFormat.Format32bppArgb);
-                TempBitmap = new Bitmap(BackgroundBitmap.Width, BackgroundBitmap.Height, PixelFormat.Format32bppArgb);
-
-                BackgroundImage.Source = BackgroundBitmap.ToImageSource();
-                MaskImage.Source = MaskBitmap.ToImageSource();
-                TempImage.Source = TempBitmap.ToImageSource();
+                ReinitializeMats();
+                Redraw();
+               
             }
             catch (Exception e)
             {
@@ -31,9 +26,32 @@ namespace ContourMaker
             }
         }
 
-        public Bitmap BackgroundBitmap { get; set; }
-        public Bitmap MaskBitmap { get; set; }
-        public Bitmap TempBitmap { get; set; }
+        private int[] Sizes { get; set; }
+
+        public Mat BackgroundBitmap { get; set; }
+        public Mat MaskBitmap { get; set; }
+        public Mat TempBitmap { get; set; }
+
+
+        private void ReinitializeMats()
+        {
+            BackgroundBitmap = new Mat("Images/test1a.jpg");
+            Sizes = new[] {BackgroundBitmap.Size(0), BackgroundBitmap.Size(1)};
+            MaskBitmap = new Mat(Sizes, BackgroundBitmap.Type(), new Scalar(0, 0, 0, 0));
+            TempBitmap = new Mat(Sizes, BackgroundBitmap.Type(), new Scalar(0, 0, 0, 0));
+
+            Cv2.Rectangle(MaskBitmap,new Rect(10,10,100,100),new Scalar(255,255,255,255),3 );
+        }
+
+        private void Redraw()
+        {
+            var tempMat = new Mat();
+            var tempMat2 = new Mat();
+            Cv2.Add(BackgroundBitmap,MaskBitmap,tempMat);
+            Cv2.Add(tempMat,TempBitmap, tempMat2);
+
+            PreviewImage.Source = tempMat2.ToBitmapSource();
+        }
 
         private void TempImage_OnMouseUp(object sender, MouseButtonEventArgs e)
         {
