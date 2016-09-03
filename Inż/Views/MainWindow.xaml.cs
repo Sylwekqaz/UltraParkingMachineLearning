7b51f3aff138fe6501b517660e88679b5efb3fcb
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-09-03 17:27:19 +0200

    reformat code

--- Inż/Views/MainWindow.xaml.cs
+++ Inż/Views/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,7 +6,6 @@ using System.Windows.Input;
 using System.Windows.Threading;
 using Inż.Model;
 using Inż.utils;
-using Ninject;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using Window = System.Windows.Window;
@@ -21,28 +18,21 @@ namespace Inż.Views
     public partial class MainWindow : Window
     {
         private readonly bool _initializing = true;
-        private readonly DispatcherTimer dispatcherTimer = new DispatcherTimer(); // get progress every second
-        private readonly FrameSource frameSourceCamera = Cv2.CreateFrameSource_Camera(1);
 
-        public MainWindow()
+        private readonly DbContext _db;
+        private readonly DispatcherTimer _dispatcherTimer = new DispatcherTimer(); // get progress every second
+        private readonly FrameSource _camera = Cv2.CreateFrameSource_Camera(1);
+
+        public MainWindow(DbContext db)
         {
-            try
-            {
-                InitializeComponent();
-                _initializing = false;
+            _db = db;
+            InitializeComponent();
+            _initializing = false;
 
-                dispatcherTimer.Tick += dispatcherTimer_Tick;
-                dispatcherTimer.Interval = new TimeSpan(1000);
-                dispatcherTimer.Start();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            _dispatcherTimer.Tick += dispatcherTimer_Tick;
+            _dispatcherTimer.Interval = new TimeSpan(1000);
+            _dispatcherTimer.Start();
         }
-
-        [Inject]
-        public DbContext Db { get; set; }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
@@ -51,12 +41,12 @@ namespace Inż.Views
         }
 
 
-        private void Redraw(bool full = false)
+        private void Redraw()
         {
             var frame = new Mat();
-            frameSourceCamera.NextFrame(frame);
+            _camera.NextFrame(frame);
 
-            var pts = Db.Contours.FindAll().ToArray();
+            var pts = _db.Contours.FindAll().ToArray();
             var mask = Gu.GetMask(pts, frame.GetSizes());
             switch ((int) ImgTypeSlider.Value)
             {
@@ -72,39 +62,34 @@ namespace Inż.Views
 
         private void AddCountur_Click(object sender, RoutedEventArgs e)
         {
-            Db.Contours.Insert(new Contour());
+            _db.Contours.Insert(new Contour());
         }
 
         private void CleatLastPoint_Click(object sender, RoutedEventArgs e)
         {
-            var lastSet = Db.Contours.FindAll().Last();
-            if (!lastSet.Pts.Any()) return;
+            var lastSet = _db.Contours.FindAll().LastOrDefault();
+            if (!(lastSet?.Pts?.Any() ?? false)) return;
             lastSet.Pts.RemoveAt(lastSet.Pts.Count - 1);
-            Db.Contours.Update(lastSet);
-            Redraw(true);
+            _db.Contours.Update(lastSet);
         }
 
         private void CleatLastCountur_Click(object sender, RoutedEventArgs e)
         {
-            if (Db.Contours.Count() == 0) return;
-            var lastSet = Db.Contours.FindAll().Last();
-            Db.Contours.Delete(lastSet.Id);
-            Redraw(true);
+            var lastSet = _db.Contours.FindAll().LastOrDefault();
+            if (lastSet != null) _db.Contours.Delete(lastSet.Id);
         }
 
         private void PreviewImage_OnMouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (Db.Contours.Count() == 0)
+            if (_db.Contours.Count() == 0)
             {
-                Db.Contours.Insert(new Contour());
+                _db.Contours.Insert(new Contour());
             }
 
             var position = e.GetPointRelativeToSource((Image) sender);
-            var contour = Db.Contours.FindAll().Last();
+            var contour = _db.Contours.FindAll().Last();
             contour.Pts.Add(position);
-            Db.Contours.Update(contour);
-
-            Redraw(true);
+            _db.Contours.Update(contour);
         }
     }
 }
