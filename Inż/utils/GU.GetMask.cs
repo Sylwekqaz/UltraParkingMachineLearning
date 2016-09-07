7db18ccfb2a7d5ba8cb41428ecee78a64546cebd
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-09-07 17:31:46 +0200

    przeniesienie wyliczania maski do osobnego pliku

---
+++ Inż/utils/GU.GetMask.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Inż.Model;
+using OpenCvSharp;
+
+namespace Inż.utils
+{
+    internal static partial class Gu
+    {
+        public static Mat GetMask(Contour[] pts, int[] sizes, Scalar color, Scalar background)
+        {
+            var ptss = pts.Where(c => c.Pts.Count > 0).Select(c => c.Pts.Select(p => (Point)p)).ToArray();
+            var ret = new Mat(sizes, MatType.CV_8UC3, background);
+
+            Cv2.FillPoly(ret, ptss, color);
+            return ret;
+        }
+
+        public static Mat GetMask(Contour[] pts, int[] sizes, Scalar color)
+        {
+            return GetMask(pts, sizes, color, Scalar.Black);
+        }
+
+        public static Mat GetMask(Contour contour, int[] sizes, Scalar color)
+        {
+            return GetMask(new[] { contour }, sizes, color);
+        }
+
+        public static int[] GetSizes(this Mat mat)
+        {
+            return new[] { mat.Size(0), mat.Size(1) };
+        }
+
+        public static Mat GetMask(Contour contour, int[] sizes, Scalar color, Scalar background)
+        {
+            return GetMask(new[] { contour }, sizes, color, background);
+        }
+
+    }
+}
--- Inż/utils/GU.cs
+++ Inż/utils/GU.cs
@@ -5,32 +5,8 @@ using OpenCvSharp;
 
 namespace Inż.utils
 {
-    internal static class Gu
+    internal static partial class Gu
     {
-        public static Mat GetMask(Contour[] pts, int[] sizes, Scalar color)
-        {
-            return GetMask(pts, sizes, color, Scalar.Black);
-        }
-
-        public static Mat GetMask(Contour[] pts, int[] sizes, Scalar color, Scalar background)
-        {
-            var ptss = pts.Where(c => c.Pts.Count > 0).Select(c => c.Pts.Select(p => (Point) p)).ToArray();
-            var ret = new Mat(sizes, MatType.CV_8UC3, background);
-
-            Cv2.FillPoly(ret, ptss, color);
-            return ret;
-        }
-
-        public static Mat GetMask(Contour contour, int[] sizes, Scalar color)
-        {
-            return GetMask(new[] { contour }, sizes, color);
-        }
-
-        public static int[] GetSizes(this Mat mat)
-        {
-            return new[] {mat.Size(0), mat.Size(1)};
-        }
-
         public static Mat AddLayers(params Mat[] mats)
         {
             if (mats.Length == 1)
@@ -85,11 +61,6 @@ namespace Inż.utils
             }
 
             return white/all >0.1;
-        }
-
-        public static Mat GetMask(Contour contour, int[] sizes, Scalar color, Scalar background)
-        {
-            return GetMask(new[] {contour}, sizes, color, background);
         }
     }
 }
