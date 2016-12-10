ecef0f7b54e0143548097ac25aa6f1224153a7b9
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-12-10 22:34:07 +0100

    dodanie biblioteki frody, implementacja wyboru zdjęcia

--- src/PrepareData/ViewModels/ImageVM.cs
+++ src/PrepareData/ViewModels/ImageVM.cs
@@ -1,10 +1,36 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Logic.Model;
+using Newtonsoft.Json;
+using PropertyChanged;
 
 namespace PrepareData.ViewModels
 {
+    [ImplementPropertyChanged]
     public class ImageVM
     {
+        public ImageVM(string imagePath)
+        {
+            ImagePath = imagePath;
+
+            if (File.Exists(JsonPath))
+            {
+                ParkingSlots =
+                    new ObservableCollection<ParkingSlot>(
+                        JsonConvert.DeserializeObject<List<ParkingSlot>>(File.ReadAllText(JsonPath)));
+            }
+            else
+            {
+                ParkingSlots = new ObservableCollection<ParkingSlot>();
+            }
+
+        }
+
         public string ImagePath { get; set; }
         public string Name => Path.GetFileNameWithoutExtension(ImagePath);
+        public string JsonPath => Path.ChangeExtension(ImagePath, ".json");
+        public ObservableCollection<ParkingSlot> ParkingSlots { get; set; }
     }
 }
--- src/PrepareData/ViewModels/MainWindowVM.cs
+++ src/PrepareData/ViewModels/MainWindowVM.cs
@@ -2,10 +2,12 @@
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using PropertyChanged;
 
 namespace PrepareData.ViewModels
 {
-    public class MainWindowVM
+    [ImplementPropertyChanged]
+    public class MainWindowVM 
     {
         public MainWindowVM()
         {
@@ -14,14 +16,13 @@ namespace PrepareData.ViewModels
             var files = Directory.EnumerateFiles(Properties.Settings.Default.DataSetPath)
                 .Where(path => extensions
                     .Any(ext => ext.Equals(Path.GetExtension(path), StringComparison.InvariantCultureIgnoreCase)))
-                .Select(path => new ImageVM()
-                {
-                    ImagePath = path,
-                });
+                .Select(path => new ImageVM(path));
 
             Images = new ObservableCollection<ImageVM>(files);
         }
 
         public ObservableCollection<ImageVM> Images { get; set; }
+
+        public ImageVM SelectedImage { get; set; }
     }
 }
