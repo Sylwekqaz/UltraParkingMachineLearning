4bbfbe7a6fc6a1d744c2cf493b88389f51acd30b
Sylwekqaz <Sylwekqaz19@gmail.com>
2016-12-19 14:10:08 +0100

    dodawanie nowego konturu przez przycisk

--- src/PrepareData/ViewModels/MainWindowVM.cs
+++ src/PrepareData/ViewModels/MainWindowVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using PrepareData.Utils;
 using PropertyChanged;
 
 namespace PrepareData.ViewModels
@@ -22,11 +23,25 @@ namespace PrepareData.ViewModels
 
             SelectedImage = Images.FirstOrDefault();
             SelectedSlot = SelectedImage?.ParkingSlots.FirstOrDefault();
+
+            AddContour = new RelayCommand<object>(AddContourhandler, CanAddContourhandler);
         }
 
         public ObservableCollection<ImageVM> Images { get; set; }
 
         public ImageVM SelectedImage { get; set; }
         public ParkingSlotVM SelectedSlot { get; set; }
+
+        public RelayCommand<object> AddContour { get; }
+
+        private bool CanAddContourhandler(object o)
+        {
+            return SelectedImage != null;
+        }
+
+        private void AddContourhandler(object o)
+        {
+            SelectedImage.ParkingSlots.Add(new ParkingSlotVM());
+        }
     }
 }
