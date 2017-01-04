0291b27655f806490c408c106ad37539be4705e2
Sylwekqaz <Sylwekqaz19@gmail.com>
2017-01-04 21:50:14 +0100

    Wyjęcie do jednego miejsca definiowania cech

--- src/Ultra.MachineLearning/ImageFeaturesExtensions.cs
+++ src/Ultra.MachineLearning/ImageFeaturesExtensions.cs
@@ -31,34 +31,14 @@ namespace Ultra.MachineLearning
 
         public static Mat ToPredictionMat(this ImageFeatures features)
         {
-            float[] sample =
-            {
-                features.SaturatedPixelsRatio,
-                features.EdgePixelsRatio,
-                features.SaturationMean,
-                features.SaturationStddev,
-                features.ValueMean,
-                features.ValueStddev
-            };
-            return new Mat(1, 6, MatType.CV_32FC1, sample);
+            var array = features.ToArray();
+            return new Mat(1, array.Length, MatType.CV_32FC1, array);
         }
 
         public static Mat ToTrainingMat(this List<ImageFeatures> features)
         {
-            var count = features.Count;
-
-            float[,] sample = new float[count, 6];
-            for (int i = 0; i < count; i++)
-            {
-                sample[i, 0] = features[i].SaturatedPixelsRatio;
-                sample[i, 1] = features[i].EdgePixelsRatio;
-                sample[i, 2] = features[i].SaturationMean;
-                sample[i, 3] = features[i].SaturationStddev;
-                sample[i, 4] = features[i].ValueMean;
-                sample[i, 5] = features[i].ValueStddev;
-            }
-
-            return new Mat(count, 6, MatType.CV_32FC1, sample);
+            var array = features.Select(ToArray).To2D();
+            return new Mat(array.GetLength(0), array.GetLength(1), MatType.CV_32FC1, array);
         }
 
         public static Mat ToResponseMat(this List<ImageFeatures> features)
@@ -66,6 +46,41 @@ namespace Ultra.MachineLearning
             int[] responses = features.Select(f => Convert.ToInt32(f.IsOccupied)).ToArray();
 
             return new Mat(responses.Length, 1, MatType.CV_32SC1, responses);
+        }
+
+        private static float[] ToArray(this ImageFeatures features)
+        {
+            return new[]
+            {
+                features.SaturatedPixelsRatio,
+                features.EdgePixelsRatio,
+                features.SaturationMean,
+                features.SaturationStddev,
+                features.ValueMean,
+                features.ValueStddev,
+            };
+        }
+
+        static float[,] To2D(this IEnumerable<float[]> source)
+        {
+            try
+            {
+                List<float[]> list = source as List<float[]> ?? source.ToList();
+
+                int firstDim = list.Count();
+                int secondDim = list.GroupBy(row => row.Length).Single().Key; // throws InvalidOperationException if source is not rectangular
+
+                var result = new float[firstDim, secondDim];
+                for (int i = 0; i < firstDim; ++i)
+                    for (int j = 0; j < secondDim; ++j)
+                        result[i, j] = list[i][j];
+
+                return result;
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException("The given jagged array is not rectangular.");
+            }
         }
     }
 }
