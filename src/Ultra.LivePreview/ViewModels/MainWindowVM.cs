802f4a947983d1de1a95e5882857cc30f9716626
Sylwekqaz <sylwekqaz19@gmail.com>
2017-05-16 17:41:57 +0200

    Dodanie projektu LivePreview opartego na kodzie PrepareData

---
+++ src/Ultra.LivePreview/ViewModels/MainWindowVM.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using PropertyChanged;
+using Ultra.Contract.Model;
+using Ultra.IO;
+using Ultra.PrepareData.Utils;
+using Ultra.PrepareData.ViewModels;
+
+namespace Ultra.LivePreview.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class MainWindowVM
+    {
+        public MainWindowVM()
+        {
+            AddContour = new RelayCommand<object>(AddSlotHandler, CanAddContour);
+            DeleteContour = new RelayCommand<ParkingSlotVM>(DeleteSlotHandler);
+            SaveToFile = new RelayCommand<object>(SaveToFileHandler);
+            MarkEmptySlot = new RelayCommand<ParkingSlotVM>(o => MarkSlot(o, occupied: false));
+            MarkOccupiedSlot = new RelayCommand<ParkingSlotVM>(o => MarkSlot(o, occupied: true));
+            MoveNext = new RelayCommand<object>(o =>
+            {
+                var i = Images.IndexOf(SelectedImage);
+                SelectedImage = Images[++i % Images.Count];
+            });
+            MovePrev = new RelayCommand<object>(o =>
+            {
+                var i = Images.IndexOf(SelectedImage);
+                SelectedImage = Images[(--i + Images.Count) % Images.Count];
+            });
+        }
+
+
+        public ObservableCollection<ImageVM> Images { get; set; }
+
+        public ImageVM SelectedImage { get; set; }
+        public ParkingSlotVM SelectedSlot { get; set; }
+
+
+        public RelayCommand<object> AddContour { get; }
+        public RelayCommand<ParkingSlotVM> DeleteContour { get; }
+
+        public RelayCommand<object> SaveToFile { get; }
+
+        public RelayCommand<ParkingSlotVM> MarkEmptySlot { get; }
+
+        public RelayCommand<ParkingSlotVM> MarkOccupiedSlot { get; }
+
+        public RelayCommand<object> MoveNext { get; }
+        public RelayCommand<object> MovePrev { get; }
+
+
+        private void AddSlotHandler(object o)
+        {
+            SelectedImage.ParkingSlots.Add(new ParkingSlotVM());
+            SelectedSlot = SelectedImage.ParkingSlots.LastOrDefault();
+        }
+
+        private void DeleteSlotHandler(ParkingSlotVM parkingSlotVM)
+        {
+            var index = SelectedImage.ParkingSlots.IndexOf(parkingSlotVM);
+            SelectedImage.ParkingSlots.Remove(parkingSlotVM);
+            SelectedSlot = index == -1
+                ? null
+                : SelectedImage.ParkingSlots.Count <= index
+                    ? SelectedImage.ParkingSlots.LastOrDefault()
+                    : SelectedImage.ParkingSlots[index];
+        }
+
+        private bool CanAddContour(object o)
+        {
+            return SelectedImage != null;
+        }
+
+        public void SaveToFileHandler(object o)
+        {
+            foreach (var imageVM in Images)
+            {
+                var data = imageVM.ParkingSlots
+                    .Select(slot => new ParkingSlot()
+                    {
+                        IsOccupied = slot.IsOccupied,
+                        Contour = new Contour(slot.Pts
+                            .Select(point => new Contour.Point()
+                            {
+                                X = point.X,
+                                Y = point.Y,
+                            }))
+                    });
+
+                FeatureLoader.SaveSlots(imageVM.ImagePath,data);
+            }
+        }
+
+        public void LoadFromFile(string directoryPath)
+        {
+            var files = FeatureLoader.GetPhotos(directoryPath)
+                .Select(path => new ImageVM(path));
+
+            Images = new ObservableCollection<ImageVM>(files);
+
+            SelectedImage = Images.FirstOrDefault();
+            SelectedSlot = SelectedImage?.ParkingSlots?.FirstOrDefault();
+        }
+
+        private void MarkSlot(ParkingSlotVM slotVM, bool occupied)
+        {
+            if (slotVM == null) return;
+            slotVM.IsOccupied = occupied;
+        }
+    }
+}
---
+++ src/Ultra.LivePreview/Views/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WinForms = System.Windows.Forms;
+using Ultra.PrepareData.ViewModels;
+
+namespace Ultra.LivePreview.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData(askForSave: false);
+            SetScale();
+        }
+
+        private void ImageListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+            listBox?.ScrollIntoView(listBox.SelectedItem);
+            SetScale();
+        }
+
+        private void LoadDataMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData(bool askForSave = true)
+        { 
+            if (askForSave)
+            {
+                var result = MessageBox.Show("Czy chcesz zapisać dane", "Możliwa utrata danych", MessageBoxButton.YesNoCancel);
+                switch (result)
+                {
+                    case MessageBoxResult.None:
+                    case MessageBoxResult.Cancel:
+                        return;
+                    case MessageBoxResult.Yes:
+                        ((MainWindowVM) DataContext).SaveToFileHandler(null);
+                        break;
+                    case MessageBoxResult.No:
+                        //nothing to do, just continue
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            using (var fbd = new WinForms.FolderBrowserDialog() {SelectedPath = Directory.GetCurrentDirectory()})
+            {
+                if (fbd.ShowDialog() == WinForms.DialogResult.OK)
+                {
+                    ((ViewModels.MainWindowVM) DataContext).LoadFromFile(fbd.SelectedPath);
+                }
+            }
+        }
+
+        private void SetScale()
+        {
+            var child = VisualTreeHelper.GetChild(Viewbox, 0) as ContainerVisual;
+            var scaleTransform = child?.Transform as ScaleTransform;
+
+            Manipulator.Scale = scaleTransform?.ScaleX ?? 1;
+
+        }
+
+        private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SetScale();
+        }
+    }
+}
